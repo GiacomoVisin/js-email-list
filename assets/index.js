e30fbd35d0f2569de4418505db665e7fa1337aac
GiacomoVisin <killerboss1150@gmail.com>
2026-03-31 21:32:18 +0200

    first request with js

--- assets/index.js
+++ assets/index.js
@@ -3,3 +3,17 @@ console.log(`hello`);
 
 const listEl = document.getElementById(`list`)
 
+const api = `https://flynn.boolean.careers/exercises/api/random/mail`
+
+
+
+for (let i = 0; i <= 10; i++) {
+    const liEl = document.createElement(`li`)
+    fetch(api)
+        .then(response => response.json())
+        .then(data => {
+            liEl.innerHTML = data.response
+            listEl.append(liEl)
+        })
+
+}
